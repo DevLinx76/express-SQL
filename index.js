cc3ff9e0ac4bce16a3481958ddf71666e86a6440
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-03-07 00:04:38 -0500

    edited index.js

--- index.js
+++ index.js
@@ -63,30 +63,34 @@ const init = async() => {
     // Drop tables
     let SQL = ``;
     await client.query(SQL);
-    console.log('tables created');
-    
-    // Seed data
-    SQL = ` `;
-    await client.query(SQL);
-    console.log('data seeded'); 
+    console.log('tables created')    
+     
+    // Start the server
+    SQL = `     
+        DROP TABLE IF EXISTS notes;
+
+        CREATE TABLE notes(
+        id SERIAL PRIMARY KEY,
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now(),
+        ranking INTEGER DEFAULT 3 NOT NULL,
+        txt VARCHAR(255) NOT NULL 
+    );`
+
+     // Seed data
+    SQL = ` 
+        INSERT INTO notes(txt, ranking) VALUES('learn express', 5);
+        INSERT INTO notes(txt, ranking) VALUES('write SQL queries', 4);
+        INSERT INTO notes(txt, ranking) VALUES('create routes', 2);
+    `
+    await client.query(SQL);    
+    console.log('data seeded');    
     
     // Start the server
-    SQL = ` 
-    DROP TABLE IF EXISTS notes;
-
-    CREATE TABLE notes(
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT now(),
-    updated_at TIMESTAMP DEFAULT now(),
-    ranking INTEGER DEFAULT 3 NOT NULL,
-    txt VARCHAR(255) NOT NULL 
-    );
-
-    INSERT INTO notes(txt, ranking) VALUES('learn express', 5);
-    INSERT INTO notes(txt, ranking) VALUES('write SQL queries', 4);
-    INSERT INTO notes(txt, ranking) VALUES('create routes', 2);
-
-    `;
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    });
 };
 
 
