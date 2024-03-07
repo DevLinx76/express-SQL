e9e644390d902e7cefb74b31bbc95662bc9c07d0
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-03-06 23:35:29 -0500

    modified index.js

--- index.js
+++ index.js
@@ -1,21 +1,76 @@
+// Purpose: to create a server that will allow the user to interact with a database of notes.
 const pg = require('pg');
 const express = require('express');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db');
 const app = express();
 
+// Middleware
+app.use(express.json());
 
+// Logging
+app.use(require('morgan')('dev'));
+
+// Get all notes
+app.get('/api/notes', async (req, res, next) => {
+    try {
+        const SQL = 'SELECT * from notes ORDER BY created_at DESC;'
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Update a note
+app.post('/api/notes', async (req, res, next) => {
+    try {
+        const SQL = 'INSERT INTO notes(txt, ranking) VALUES($1, $2) RETURNING *';
+        const response = client.query(SQL, [req.body.txt]);
+        res.send(response.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Update a note
+app.put('/api/notes/:id', async (req, res, next) => {
+    try {
+        const SQL = 'UPDATE notes SET txt=$1, ranking=$2, update_at=now(WHERE id=$3 RETURNING *) WHERE id=$3 RETURNING *';
+        const response = client.query(SQL, [req.body.txt, req.body.ranking, req.params.id]);
+        res.send(response.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Delete a note
+app.delete('/api/notes/:id', async (req, res, next) => {
+    try {
+        const SQL = 'DELETE FROM notes WHERE id=$1 RETURNING *';
+        const response = client.query(SQL, [req.params.id]);
+        res.sendStatus(204);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+// Error handling
 const init = async() => {
     await client.connect();
     console.log('connected to the database');
     
+    // Drop tables
     let SQL = ``;
     await client.query(SQL);
     console.log('tables created');
     
+    // Seed data
     SQL = ` `;
     await client.query(SQL);
     console.log('data seeded'); 
     
+    // Start the server
     SQL = ` 
     DROP TABLE IF EXISTS notes;
 
@@ -32,51 +87,6 @@ const init = async() => {
     INSERT INTO notes(txt, ranking) VALUES('create routes', 2);
 
     `;
-
-    app.use(express.json());
-    app.use(require('morgan')('dev'));
-
-    app.get('/api/notes', async (req, res, next) => {
-        try {
-            const SQL = 'SELECT * from notes ORDER BY created_at DESC;'
-            const response = await client.query(SQL);
-            res.send(response.rows);
-        } catch (err) {
-            next(err);
-        }
-    });
-
-    app.post('/api/notes', async (req, res, next) => {
-        try {
-            const SQL = 'INSERT INTO notes(txt, ranking) VALUES($1, $2) RETURNING *';
-            const response = client.query(SQL, [req.body.txt]);
-            res.send(response.rows[0]);
-        } catch (err) {
-            next(err);
-        }
-    });
-    
-    app.put('/api/notes/:id', async (req, res, next) => {
-        try {
-            const SQL = 'UPDATE notes SET txt=$1, ranking=$2, update_at=now(WHERE id=$3 RETURNING *) WHERE id=$3 RETURNING *';
-            const response = client.query(SQL, [req.body.txt, req.body.ranking, req.params.id]);
-            res.send(response.rows[0]);
-        } catch (err) {
-            next(err);
-        }
-    });
-    app.delete('/api/notes/:id', async (req, res, next) => {
-        try {
-            const SQL = 'DELETE FROM notes WHERE id=$1 RETURNING *';
-            const response = client.query(SQL, [req.params.id]);
-            res.sendStatus(204);
-        } catch (err) {
-            next(err);
-        }
-    });
-
-
-
 };
 
 
